27a1c14a05fa55b7a277c60de3f60bf7845d8ae0
dimryb <dimryb@bk.ru>
2022-07-04 16:45:02 +0700

    Карированные функции, смена местами аргументов карированной функции.

--- src/nativeMain/kotlin/Caried.kt
+++ src/nativeMain/kotlin/Caried.kt
@@ -1,12 +1,17 @@
+fun add1(a: Int): (Int) -> Int = { b -> a + b }
+fun add11(a: Int): (Int) -> Int {
+    return { a + it }
+}
 
-fun add1(a: Int) : (Int) -> Int = { b -> a + b}
-val add2: (Int) -> (Int) -> Int = { a -> { b -> a + b }}
+
+val add2: (Int) -> (Int) -> Int = { a -> { b -> a + b } }
 
 typealias IntBinOp = (Int) -> (Int) -> Int
-val add: IntBinOp = { a -> { b -> a + b }}
-val mult: IntBinOp = { a -> { b -> a * b }}
 
-fun printCurriedResult(){
+val add: (Int) -> ((Int) -> Int) = { a -> { b -> a + b } }
+val mult: IntBinOp = { a -> { b -> a * b } }
+
+fun printCurriedResult() {
     println("add1(3)(5): ${add1(3)(5)}")
     println("add2(3)(5): ${add2(3)(5)}")
     println("add(3)(5): ${add(3)(5)}")
